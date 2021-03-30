b16600f5e04f654a1a67db59cba284dc9e7333b9
catherinedhwang <catherine.d.hwang@gmail.com>
2021-03-30 16:13:16 -0400

    first portion of UFO 1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
+console.log(data);
 
-// YOUR CODE HERE!
+// Reference the table body
+var tbody = d3.select("tbody");
+
+// Iterate through every object in data
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    // Append "tr"
+    var row = tbody.append("tr");
+    //  Object.entries will console.log each object
+    Object.entries(ufoReport).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append "td" for each value
+        var cell = row.append("td");
+        // Print value as text
+        cell.text(value);
+    });
+});
