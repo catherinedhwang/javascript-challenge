3fbfebd0096d26f5c0ed2b03fa56dc109d03a14f
catherinedhwang <catherine.d.hwang@gmail.com>
2021-03-31 08:49:30 -0400

    level 1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,17 +1,62 @@
-// from data.js
+// LEVEL 1
+// // from data.js
 var tableData = data;
-console.log(data);
+// console.log(data);
 
 // Reference the table body
 var tbody = d3.select("tbody");
 
-// Iterate through every object in data
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
-    // Append "tr"
+// // Iterate through every object in data in CALLBACK
+// data.forEach(function(populateTable) {
+//     console.log(populateTable);
+//     // Append "tr"
+//     var row = tbody.append("tr");
+//     //  Object.entries will console.log each object
+//     Object.entries(populateTable).forEach(function([key, value]) {
+//         console.log(key, value);
+//         // Append "td" for each value
+//         var cell = row.append("td");
+//         // Print value as text
+//         cell.text(value);
+//     });
+// });
+
+function populateTable(data) {
     var row = tbody.append("tr");
-    //  Object.entries will console.log each object
-    Object.entries(ufoReport).forEach(function([key, value]) {
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+}
+
+tableData.forEach(populateTable);
+
+// Select the button
+var button = d3.select("#filter-btn");
+// Define function 
+button.on("click", function() {
+
+    tbody.html("");
+
+    // Select the input date get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach(function(filters) {
+
+    console.log(filters);
+    
+    // Append "tr" for filtered data
+    var row = tbody.append("tr");
+    // Object.entries will console.log each object in filtered data
+    Object.entries(filters).forEach(function([key, value]) {
         console.log(key, value);
         // Append "td" for each value
         var cell = row.append("td");
@@ -19,3 +64,5 @@ data.forEach(function(ufoReport) {
         cell.text(value);
     });
 });
+});
+
